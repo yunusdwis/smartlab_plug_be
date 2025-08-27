76442f349c839c8b1eff0386e30430e15623a26a
cchasyz <hasandocs2@gmail.com>
2025-08-27 16:48:51 +0700

    changed route api

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ $router->post('/register', 'AuthController@register');
 $router->post('/login', 'AuthController@login');
 
 $router->group(['middleware' => 'authToken'], function () use ($router) {
-  $router->get('/led/on', 'Controller@turnOn');
-  $router->get('/led/off', 'Controller@turnOff');
+  $router->get('/socket/on', 'Controller@turnOn');
+  $router->get('/socket/off', 'Controller@turnOff');
   $router->get('/config_wifi', 'Controller@startWifiConfig');
   $router->post('/logout', 'AuthController@logout');
 });
