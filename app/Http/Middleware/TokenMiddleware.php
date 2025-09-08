b738dcc3e2a8d89fabdc0e44ca8b8df755718fcd
cchasyz <hasandocs2@gmail.com>
2025-09-08 11:58:36 +0700

    added dual auth

--- app/Http/Middleware/TokenMiddleware.php
+++ app/Http/Middleware/TokenMiddleware.php
@@ -10,6 +10,27 @@ class TokenMiddleware
     public function handle($request, Closure $next)
     {
         $token = $request->bearerToken(); 
+
+        $url = "https://v3.boothlab.id/api/check_token/$token";
+        $opts = [
+            "http" => [
+                "method"  => "GET",
+                "header"  => "Authorization: Bearer $token\r\n" .
+                            "Accept: application/json\r\n"
+            ]
+        ];
+
+        $context = stream_context_create($opts);
+        $result  = @file_get_contents($url, false, $context);
+
+        $statusLine = $http_response_header[0] ?? "HTTP/1.1 500 Internal Server Error";
+        preg_match('{HTTP/\S+ (\d{3})}', $statusLine, $match);
+        $statusCode = $match[1] ?? 500;
+
+        if ($statusCode == 200){
+            return $next($request);
+        } 
+
         $user = User::where('token', $token)->first();
 
         if (!$token || !$user) {
